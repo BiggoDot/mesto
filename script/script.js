0643f1b9f3e4d74435c21476ffc5ae7bc7831357
BiggoDot <ovl1work2@gmail.com>
2021-12-17 20:17:31 +0200

    hotfix/after-review

--- script/script.js
+++ script/script.js
@@ -1,32 +1,31 @@
-const popups = document.querySelector('.popup');
-const cpopup = popups.querySelector('.popup__close');
-const opopup = document.querySelector('.profile__edit');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.big-container__close');
+const popupOpenButton = document.querySelector('.profile__edit');
 const saveButton = document.querySelector('.form__save');
 const bio = document.querySelector('.profile__info');
-const form = document.querySelector('.form__input');
+const form = document.querySelector('.form');
 const name = document.getElementById('name');
 const description = document.getElementById('description');
+const profileDescription = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
 
-function pop() {
-  popups.classList.add('popup_opened');
-  name.value = document.querySelector('.profile__name').textContent;
-  description.value = document.querySelector('.profile__description').textContent;
+function popupOpen() {
+  name.value = profileName.textContent;
+  description.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
 };
 
-function close() {
-  popups.classList.remove('popup_opened');
+function popupClose() {
+  popup.classList.remove('popup_opened');
 };
 
-function saveName(evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
-  bio.innerHTML = `
-  <h1 class="profile__name">${name.value}</h1>
-  <p class="profile__description">${description.value}</p>`;
-  name.value = document.querySelector('.profile__name').textContent;
-  description.value = document.querySelector('.profile__description').textContent;
+  profileName.textContent = name.value;
+  profileDescription.textContent = description.value;
+  popupClose();
 };
 
-cpopup.addEventListener('click', close);
-opopup.addEventListener('click', pop);
-form.addEventListener('submit', saveName);
-saveButton.addEventListener('click', close);
+popupCloseButton.addEventListener('click', popupClose);
+popupOpenButton.addEventListener('click', popupOpen);
+form.addEventListener('submit', formSubmitHandler);
